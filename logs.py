6e6af4849159f0d0e756211a0b232f5b50b8d51b
Rohan Garner <161069703+08BitPixels@users.noreply.github.com>
2025-04-02 20:22:15 +0100

    Update logs.py
    - fixed it so all uncaught exceptions are logged properly
    - corrected the get_logger() function to properly return a unique logger

--- logs.py
+++ logs.py
@@ -1,53 +1,59 @@
-import loguru
+import logging
 import datetime
 import os
 import sys
 
-print('logger init')
-logger = loguru.logger
-format = '{time} {level} {message}'
+LOG_FORMAT = '[%(asctime)s] [%(name)s/ %(funcName)s] [%(levelname)s]: %(message)s'
+TIME_FORMAT = '%H:%M:%S'
+TODAY = datetime.date.today().strftime('%d-%m-%y')
+data: dict[str, int | str] = {}
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def save_path(relative_path: str) -> str:
+
+	if getattr(sys, '_MEIPASS', False):
+		path = os.path.join(BASE_PATH, '..', relative_path)
+	
+	else:
+		path = os.path.join(BASE_PATH, relative_path)
+
+	return path
+
+DIR_PATH = save_path(os.path.join('logs', TODAY))
+os.makedirs(DIR_PATH, exist_ok = True)
+runs = len(os.listdir(DIR_PATH)) + 1
+FILE_PATH = os.path.join(DIR_PATH, f'{runs}.log')
+
+FORMATTER = logging.Formatter(
+	fmt = LOG_FORMAT,
+	datefmt = TIME_FORMAT
+)
+
+logging.basicConfig(
+
+	filename = FILE_PATH, 
+	format = LOG_FORMAT, 
+	datefmt = TIME_FORMAT,
+	level = logging.CRITICAL
+
+)
 
 def exception(exc_type, exc_value, exc_traceback):
 	
 	sys.__excepthook__(exc_type, exc_value, exc_traceback)
-	logger.critical('\n\n', exc_info = (exc_type, exc_value, exc_traceback))
+	logging.critical('\n\n', exc_info = (exc_type, exc_value, exc_traceback))
 
-# sys.excepthook = exception
+sys.excepthook = exception
 
-today = datetime.date.today().strftime('%d-%m-%y')
-data: dict[str, int | str] = {}
-base_path = os.path.dirname(os.path.abspath(__file__))
-dir_path = os.path.join(base_path, '..' if getattr(sys, '_MEIPASS', False) else '', 'logs', today)
+def get_logger(name: str) -> logging.Logger:
 
-os.makedirs(dir_path, exist_ok = True)
-runs = len(os.listdir(dir_path)) + 1
-file_name = os.path.join(dir_path, f'{runs}.log')
+	file_handler = logging.FileHandler(filename = FILE_PATH)
+	file_handler.setFormatter(FORMATTER)
 
-if not os.path.isdir(dir_path):
-	os.makedirs(dir_path)
+	logger = logging.getLogger(name = name)
+	logger.setLevel(logging.DEBUG)
+	logger.addHandler(file_handler)
+	logger.propagate = False
+	logger.info(f'logger "{name}" initialised')
 
-def make_filter(name):
-	
-    def filter(record): return record['extra'].get('name') == name
-    return filter
-
-def get_logger(name: str) -> loguru.logger:
-	
-	# logger.add(format = f'[%(asctime)s] [{name}]/%(levelname)s]: %(message)s', filter = make_filter(name))
-
-	logger.add(
-		sink = file_name, 
-		level = 'DEBUG', 
-		format = format, 
-		filter = make_filter(name),
-		colorize = True, 
-		backtrace = True
-	)
-	new_logger = logger.bind(name = name)
-	new_logger.info(f'"{name}" logger initialised')
-
-	return new_logger
-
-logger = get_logger(name = 'logs.py') # get logger
-logger.info('initialising program...')
-pnint()
+	return logger
